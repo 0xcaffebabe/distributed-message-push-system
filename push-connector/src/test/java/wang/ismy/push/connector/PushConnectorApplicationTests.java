3278602afc617ef45837e9a7181d51a40d5bc722
MY <715711877@qq.com>
2020-06-29 16:16:26 +0800

    CI

--- push-connector/src/test/java/wang/ismy/push/connector/PushConnectorApplicationTests.java
+++ push-connector/src/test/java/wang/ismy/push/connector/PushConnectorApplicationTests.java
@@ -1,13 +1,14 @@
 package wang.ismy.push.connector;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class PushConnectorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
