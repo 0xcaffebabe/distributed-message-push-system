92f53f1b1679e3661d0d82ae999e0d13c0daaa81
MY <715711877@qq.com>
2020-07-07 15:24:47 +0800

    lookup 使用内置负载均衡策略

--- push-lookup/src/main/java/wang/ismy/push/lookup/LookupApi.java
+++ push-lookup/src/main/java/wang/ismy/push/lookup/LookupApi.java
@@ -2,6 +2,7 @@ package wang.ismy.push.lookup;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,20 +18,17 @@ import java.util.Random;
 @RestController
 public class LookupApi {
 
-    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
     private RestTemplate restTemplate;
 
-    public LookupApi(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
-        this.discoveryClient = discoveryClient;
+    public LookupApi(LoadBalancerClient loadBalancerClient, RestTemplate restTemplate) {
+        this.loadBalancerClient = loadBalancerClient;
         this.restTemplate = restTemplate;
     }
 
     @GetMapping
     public String getConnector(){
-        List<ServiceInstance> connector = discoveryClient.getInstances("connector-service");
-        Random random = new Random();
-        int i = random.nextInt(connector.size());
-        ServiceInstance service = connector.get(i);
+        ServiceInstance service = loadBalancerClient.choose("connector-service");
         return service.getHost()+":"+restTemplate.getForObject(service.getUri()+"/port",String.class);
     }
 
