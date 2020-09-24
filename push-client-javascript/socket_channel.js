769e902878adfc6f6663d255ecddc6a6b4ffdd32
0xcaffebabe <715711877@qq.com>
2020-09-24 17:49:16 +0800

    js 客户端开发

--- push-client-javascript/socket_channel.js
+++ push-client-javascript/socket_channel.js
@@ -2,16 +2,31 @@ const net = require('net')
 
 class SocketChannel {
 
+  constructor(socket,callback){
+    this.socket = socket
+    this.dataQueue = []
+    this.callback = callback
+    this.socket.on('data',data => {
+      const datas = data.toString().split('\n')
+      for(let i = 0;i<datas.length;i++){
+        if (datas[i]) {
+          this.callback(datas[i])
+        }
+      }
+    })
+  }
+
+  writeAndFlush(msg){
+    this.socket.write(msg)
+  }
 }
 
 const client = net.connect(
   {port: 80, host: 'baidu.com'},
-  () => {
-    console.log('client conencted')
-    client.write('GET / HTTP/1.1\r\nHost: www.baidu.com\r\n\r\n')
+  (e) => {
+    console.log(e)
   }
 )
 
-client.on('data', data => {
-  console.log(data.toString())
-})
+const sc = new SocketChannel(client, data => console.log(data))
+sc.writeAndFlush('GET / HTTP/1.1\r\nHost: www.baidu.com\r\n\r\n')
